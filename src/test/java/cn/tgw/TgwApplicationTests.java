f6da771d7e074d6643a08faa56391f9e6a0db7f4
TjSanshao <1305705188@qq.com>
2018-11-26 16:13:15 +0800

    项目创建完成

---
+++ src/test/java/cn/tgw/TgwApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.tgw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TgwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
