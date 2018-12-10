c38990bea7b0be1183d8068baa2c0b431c65f4ad
XiaoJian <767365673@qq.com>
2018-12-10 11:24:10 +0800

    新增订单查询接口

--- src/main/java/cn/tgw/order/controller/OrderController.java
+++ src/main/java/cn/tgw/order/controller/OrderController.java
@@ -5,11 +5,12 @@ import cn.tgw.order.model.Order;
 import cn.tgw.order.service.OrderService;
 import cn.tgw.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -51,6 +52,33 @@ public class OrderController {
 
         return createStatus;
 
+    }
+
+    /**
+     *
+     * 功能描述: 查询用户订单，根据订单状态sellStatus查询
+     *                                  sellStatus：0未付款，1已付款，2退款，3已使用，4已评论，5已过期
+     * @param: HttpSession session,int sellStatus
+     * @return:
+     * @auther: 张华健
+     * @date:  2018/12/10
+     */
+    @RequestMapping(value = "/xiaojian/order/getOrders",method = RequestMethod.GET)
+    public Object getOrders(HttpSession session,@RequestParam(value = "sellStatus",required = false)Integer sellStatus){
+        Map<String,Object> result = new HashMap<>();
+        result.put("status", "fail");
+        result.put("message", "unknown error");
+        User user = (User) session.getAttribute(TGWStaticString.TGW_USER);
+        List<Order> resultList = new ArrayList<>();
+        if (sellStatus!=null){
+            resultList = orderService.getOrdersByUserIdAndOrderSellStatusAndStatusNormal(user.getId(), sellStatus.byteValue());
+        }else {
+            resultList = orderService.getAllOrdersByUserId(user.getId());
+        }
+        result.put("status", "success");
+        result.put("message", "success");
+        result.put("orders",resultList);
+        return result;
     }
 
 }
