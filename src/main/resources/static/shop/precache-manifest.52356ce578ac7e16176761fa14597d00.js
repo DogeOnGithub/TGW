d0a2cfa011372e17566ab6da898c5a9cc3956879
XiaoJian <767365673@qq.com>
2018-12-24 08:30:00 +0800

    前端

---
+++ src/main/resources/static/shop/precache-manifest.52356ce578ac7e16176761fa14597d00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3ff73a950c7699e762d7ad01334f28",
+    "url": "/shop/static/media/login.bf3ff73a.png"
+  },
+  {
+    "revision": "2b4305fd68c19c038a99bae492588289",
+    "url": "/shop/static/media/bg.2b4305fd.jpg"
+  },
+  {
+    "revision": "3a4c97e913adeac98b80",
+    "url": "/shop/static/js/runtime~main.3a4c97e9.js"
+  },
+  {
+    "revision": "2ccc7c97f7129b0cc930",
+    "url": "/shop/static/js/main.2ccc7c97.chunk.js"
+  },
+  {
+    "revision": "f9cb0f88113f3394671c",
+    "url": "/shop/static/js/1.f9cb0f88.chunk.js"
+  },
+  {
+    "revision": "2ccc7c97f7129b0cc930",
+    "url": "/shop/static/css/main.4c8eecb7.chunk.css"
+  },
+  {
+    "revision": "f9cb0f88113f3394671c",
+    "url": "/shop/static/css/1.61d02ab7.chunk.css"
+  },
+  {
+    "revision": "bf40ddbd826b3ed7e00fd1b8161eecd7",
+    "url": "/shop/index.html"
+  }
+];
